8b8160cf4ba2aeb986e1c0a07dd64b03e74f652a
J-Mad-08 <maddox.version1@gmail.com>
2022-11-12 22:03:29 -0500

    Utilizing fetch API

--- sandbox.ts
+++ sandbox.ts
@@ -1,19 +1,5 @@
-// reading files in human readable format
-const decoder = new TextDecoder('utf-8');
-
-// const data = await Deno.readFile('readme.txt');
-// console.log(decoder.decode(data));
-
-// writing files
-// encoder
-const encoder = new TextEncoder();
-const text = encoder.encode('hello ya back');
-//await Deno.writeFile('readme.txt', text);
-
-// renaming and removing files
-// await Deno.rename('readme.txt', 'deleteme.txt');
-// await Deno.remove('deleteme.txt');
-
-// if reading from a text file
-const data = await Deno.readTextFile('readme.txt');
+// fetch api
+const res = await fetch('https://swapi.dev/api/films/');
+const data = await res.json();
+// use allow-net to run files from network
 console.log(data);
