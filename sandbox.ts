85ae02b4bbd3d7b0688a0edba87915fe3926be9a
J-Mad-08 <maddox.version1@gmail.com>
2022-11-12 22:00:48 -0500

    easier readTextFile method implementation

--- sandbox.ts
+++ sandbox.ts
@@ -1,17 +1,19 @@
 // reading files in human readable format
 const decoder = new TextDecoder('utf-8');
 
-const data = await Deno.readFile('readme.txt');
-console.log(decoder.decode(data));
+// const data = await Deno.readFile('readme.txt');
+// console.log(decoder.decode(data));
 
 // writing files
 // encoder
 const encoder = new TextEncoder();
 const text = encoder.encode('hello ya back');
-await Deno.writeFile('readme.txt', text);
+//await Deno.writeFile('readme.txt', text);
 
 // renaming and removing files
-await Deno.rename('readme.txt', 'deleteme.txt');
-await Deno.remove('deleteme.txt');
+// await Deno.rename('readme.txt', 'deleteme.txt');
+// await Deno.remove('deleteme.txt');
 
 // if reading from a text file
+const data = await Deno.readTextFile('readme.txt');
+console.log(data);
