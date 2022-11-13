d845125e67e5ad5f76b22e03a5c147dad3e42f27
J-Mad-08 <maddox.version1@gmail.com>
2022-11-12 21:51:53 -0500

    write file permissions section

--- readme.txt
+++ readme.txt
@@ -1,2 +1,1 @@
-# deno jumpstart tutorial
-All course files for the Deno Jumpstart Tutorial on the Net Ninja YouTube chanel.
+hello ya back
--- sandbox.ts
+++ sandbox.ts
@@ -3,3 +3,10 @@ const decoder = new TextDecoder('utf-8');
 
 const data = await Deno.readFile('readme.txt');
 console.log(decoder.decode(data));
+
+// writing files
+// encoder
+const encoder = new TextEncoder();
+const text = encoder.encode('hello ya back');
+await Deno.writeFile('readme.txt', text);
+
