319f5650b83d97f378e335dbe0dce0d72c79e8a5
J-Mad-08 <maddox.version1@gmail.com>
2022-11-12 22:06:50 -0500

    Add deno link

--- sandbox.ts
+++ sandbox.ts
@@ -1,4 +1,5 @@
 // fetch api
+// look at deno permissions page: https://deno.land/manual@v1.27.2/basics/permissions#permissions
 const res = await fetch('https://swapi.dev/api/films/');
 const data = await res.json();
 // use allow-net to run files from network
